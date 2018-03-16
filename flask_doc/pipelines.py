ebc77bb3228fa4a861659bda69d92f823de1a662
shaoweicode <417096554@qq.com>
2018-03-16 15:37:20 +0800

    chanllenge19

--- flask_doc/pipelines.py
+++ flask_doc/pipelines.py
@@ -4,8 +4,16 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import re
+import redis
+import json
 
 class FlaskDocPipeline(object):
     def process_item(self, item, spider):
+        item['text'] = re.sub('\s','',item['text'])
+        self.redis.lpush('flask_doc:items',dict(item))
         return item
+
+    def open_spider(self, spider):
+        # 连接数据库
+        self.redis = redis.StrictRedis(host='localhost', port=6379, db=0)
--- flask_doc/spiders/flsak.py
+++ flask_doc/spiders/flsak.py
@@ -3,14 +3,14 @@ import scrapy
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 from flask_doc.items import PageItem
-
+import re
 class FlaskSpider(scrapy.spiders.CrawlSpider):
     name = 'flask'
     allowed_domains = ['flask.pocoo.org']
     start_urls = "http://flask.pocoo.org/docs/0.12/"
-
+    # http://flask.pocoo.org/docs/0.12/
     rules = (
-        Rule("TODO: 配置 Link Extractor，及爬取链接的规则，并合理定义其他相关参数"),
+        Rule(LinkExtractor(allow=('flask.pocoo.org/docs/0.12/', )), callback='parse_item',follow=True),
     )
 
     def parse_page(self, response):
@@ -18,4 +18,7 @@ class FlaskSpider(scrapy.spiders.CrawlSpider):
         """
         TODO:补充 url 和 text 的解析规则
         """
+        item['url'] = response.url
+        item['text']= response.css('div.section::text').extract_first()
+
         yield item
